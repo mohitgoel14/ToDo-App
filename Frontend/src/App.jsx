df4364130e50dd143ef5499bd2fa0682a8d4a3cb
Mohit Goel <91262474+mohitgoel14@users.noreply.github.com>
2024-02-11 19:57:24 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { CreateTodo } from "./Components/CreateTodo";
 import { Todos } from "./Components/Todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  fetch("http://localhost:3000/todos").then(async function (res) {
-    const json = await res.json();
-    setTodos(json.todos);
-  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:3000/todos");
+      const json = await res.json();
+      setTodos(json.todos);
+    };
+
+    fetchData(); // Call the fetchData function when the component mounts
+  }, []); // Empty dependency array ensures this effect runs only once
+
   return (
     <div>
       <CreateTodo />
-      <Todos todos = {todos} />
+      <Todos todos={todos} />
     </div>
   );
 }
